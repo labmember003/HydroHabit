dea15b9d512f16b6bfd92bf7cabd4b9f1f782107
Avishisht Gupta <avishishtgupta@gmail.com>
2026-04-12 08:00:27 +0530

    Update Gradle and Kotlin plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
     packaging {
         resources {
